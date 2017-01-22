15d3528c2bd163c3ffd27ef20b0cd098020b17b3
Jannes <jay@Creatives-MacBook-Air.local>
2017-01-22 19:15:48 +0100

    Set up index and order routes + banner

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resources :orders
   resources :ingredients
   resources :hoagies
+  resources :main, only: [:show]
+  get "index", to: "main#index"  
+  get "order", to: "orders#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
